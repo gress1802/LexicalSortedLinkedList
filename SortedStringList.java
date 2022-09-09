d9860ac4eb20416b4491f0062ee0355a3484a96a
kaajc <jgress1802@gmail.com>
2022-09-08 20:35:49 -0500

    Fished around half of insert method

--- SortedStringList.java
+++ SortedStringList.java
@@ -32,7 +32,43 @@ public class SortedStringList {
         //if a string matching s is not in the list, insert s into the list maintaining the 
         //ascending lexical and descending length orders 
         //otherwise no changes to the list are made 
+        if(heads[0] == null && heads[1] == null){ //If the list is empty 
+            heads[0] = heads[1] = new Node(s,null,null); //Creates the first node in the list
+                                                                //This node is both heads
+        }else if(heads[0].next == null && heads[1].next == null ){ //If there is only one node in the list
+            //Puts the new node in list with regards to heads[0]
+            if(heads[0].data.compareTo(s)<0){ //value is - therefore the new node containing s comes after heads[0]
+                heads[0].next[0] = new Node(s,null,null);
+            }else{ //the value is + therefore the new node containing s becomes heads[0]
+                Node temp = heads[0]; //temperary value for heads[0]
+                heads[0] = new Node(s,temp,null); //Node containing s is now heads[0] and its next reference (next[0]) is the previous heads[0]
+            }  
+            //Changing to make sure heads[1] is accurate too
+            if(heads[1].next[1].data.length() > heads[1].data.length()){
+                heads[1] = heads[0].next[0];
+                heads[1].next[1] = heads[0];
+            }
+                
+            
+        
+        }else{
+            //Start by inserting changing the ascending lexical order list
+            Node temp = heads[0];
+            if(temp.data.compareTo(s)<0){//this is the case where the node containing s must be the head
 
+            }
+            else{
+                while(temp.next[0].data.compareTo(s)>0){ //all other cases
+                    temp = temp.next[0];
+                    if(temp.next[0] == null){ //end of the list
+                        temp.next[0] = new Node(s,null,null);
+                    }
+                }
+                Node temp2 = temp.next[0]; //adds node at the correct position in the list
+                temp.next[0] = new Node(s,temp2,null);
+            }
+
+        }
 
     } 
  
