c1daad7b6a6c691ce27b8a98c6f79871417ff225
Joe <gress2123@uwlax.edu>
2022-09-12 11:26:08 -0500

    changes

--- SortedStringList.java
+++ SortedStringList.java
@@ -82,8 +82,9 @@ public class SortedStringList {
                 temp0.next[0] = new Node(s,temp2,null);
                 //Now finding what next[1] will be for this node
                 Node temp3 = heads[1];
-
-                } 
+                while(temp3.next[1] != temp4){
+                    temp3 = temp3.next[1];
+                }
             }
 
         }
